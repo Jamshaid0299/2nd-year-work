f6945f6bc739f54b43d64c0049451fde049754a8
Jamshaid0299 <jamshaid0299@gmail.com>
2025-12-09 09:02:07 +0500

    meri mrzi

---
+++ java script game/game.js
@@ -0,0 +1,24 @@
+let game = "jamshaid";
+let a = prompt("Enter a correct password: ");
+
+
+if (a!=game) {
+    game = prompt('password is wrong')
+}else{
+    console.log( "password is correct");
+}
+
+
+
+// // let obj =  {
+// // name : "ali",
+// // age : 1,
+// // adress : "some address",
+// // fname: "kala",
+// // }
+// // console.log(obj.age);
+
+
+
+// let ad = ["apnacollege"];
+//  console.log(ad[2]);
